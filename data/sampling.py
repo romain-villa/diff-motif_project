73182314332b3ddfd1803feaf68c6462eec397fb
Robin-WF <119854657+Robin-WF@users.noreply.github.com>
2022-12-07 15:42:01 +0100

    Add files via upload

---
+++ data/sampling.py
@@ -0,0 +1,18 @@
+from collections import Counter 
+from Bio import SeqIO
+from random import sample
+import sys
+
+args=sys.argv[1:]
+
+file = open(args[0][:-6]+"_sampled.fasta", "w") 
+
+with open(args[0]) as f:
+	seqs = SeqIO.parse(f, "fasta")
+	samps = ((seq.name, seq.seq) for seq in  sample(list(seqs),10000))
+	for samp in samps:
+		file.write(">{}\n{}".format(*samp))
+		file.write("\n")
+
+file.close()
+
